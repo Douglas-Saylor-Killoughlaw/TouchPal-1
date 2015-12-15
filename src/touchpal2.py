75cd741e44b8ad586af25262e532d52044a12497
kenny.li <kenny.li@kenny>
2015-12-15 22:34:27 +0800

    增加提示用户名列表

--- src/touchpal2.py
+++ src/touchpal2.py
@@ -7,16 +7,19 @@ Created on 2015年11月13日
 '''
 import urllib2,time
 
-k = {1:"R4PbXzL"}
+nameList = {"liyifei":"R4PbXzL","douyahuan":"R4229Ow"}
+print "Choose the user:"
+for k,v in nameList.items():
+    print k,"\t---->\t",v
 class Touch():
-    print k[1]
     phoneNumber = None
     def __init__(self):
-        self.invitationCode = raw_input("Enter the invitation code :")
+        self.userName = raw_input("Enter the user name :")
+#         print 'invitation code:', nameList[self.userName]
         self.phoneNumberHead = raw_input("Enter the phone number head:")
-        self.start = raw_input('input start:')
-        self.end = raw_input('input end:')
-        self.url = "http://ws2.cootekservice.com/voip/tmp_invitation?invitation="+self.invitationCode+"&phone="
+        self.start = raw_input('input start:\t')
+        self.end = raw_input('input end:\t')
+        self.url = "http://ws2.cootekservice.com/voip/tmp_invitation?invitation="+nameList[self.userName]+"&phone="
     
     def createPhoneNumber(self,phoneNumberHead):
         for i in xrange(int(self.start),int(self.end)+1):
@@ -28,17 +31,23 @@ class Touch():
                 response = urllib2.urlopen(self.url+phoneNumber)
             except urllib2.HTTPError, e:
                 print e.code
+                print "some error coming ..."
         Touch.phoneNumber = phoneNumber
     def saveLog(self):
-        
-        f = open('./log.txt','a+')
-        f.write('%s\t****logs****\n'%time.strftime("%H:%M:%S",time.localtime()))
-        f.write("invitationCode: %s\n"%self.invitationCode)
-#         total = int(self.end)-int(self.start)
-        print Touch.phoneNumber
-        f.write("end phone number is: %s\n"%Touch.phoneNumber)
+        try:
+            f = open('./log.txt','a')
+            f.write('%s\t****logs****\n'%time.strftime("%H:%M:%S",time.localtime()))
+            f.write("user: %s\n"%self.userName)
+            f.write("invitationCode: %s\n"%nameList[self.userName])
+    #         total = int(self.end)-int(self.start)
+#             print Touch.phoneNumber
+            f.write("end phone number is: %s\n"%Touch.phoneNumber)
+        except Exception,e:
+            print "End by user..."
+            print e
         f.close()
         
 myTouch = Touch()
 myTouch.createPhoneNumber(myTouch.phoneNumberHead)
 myTouch.saveLog()
+print "Mission Complete!"
